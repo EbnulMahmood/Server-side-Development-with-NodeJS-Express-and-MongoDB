d62abc51678646187deacba9e22074e02093e1e8
Mirza Ebnul Mahmood Shovan <shovonmahmood67@gmail.com>
2020-10-28 23:56:00 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
+    'secretKey': '', // use your secretKey
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '313470836650722',
-        clientSecret: '11dad6dac511569d22804cc7db5d91fd'
+        clientId: '', // use your clientId
+        clientSecret: '' // use your clientSecret
     }
 }
