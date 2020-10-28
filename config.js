6c3c381925fbf38c5f1c9e9f566db648e07fb5fb
shovon <shovonmahmood67@gmail.com>
2020-10-28 23:07:55 +0600

    confusion

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '313470836650722',
+        clientSecret: '11dad6dac511569d22804cc7db5d91fd'
+    }
+}
